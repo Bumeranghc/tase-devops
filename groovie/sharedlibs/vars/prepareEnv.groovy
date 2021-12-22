ab20bda0e5dfb76c4a311505972a1c1cf0c612be
Pavel Semchenko <pavelsemch@hotmail.com>
2021-12-22 08:34:00 +0200

    Added variable

--- groovie/sharedlibs/vars/prepareEnv.groovy
+++ groovie/sharedlibs/vars/prepareEnv.groovy
@@ -1,6 +1,7 @@
 def String call() {
     MAX_MSG_LEN = 100
     def changeString = ""
+    def changeAuthor = ""
     def changeLogSets = currentBuild.changeSets
     for (int i = 0; i < changeLogSets.size(); i++) {
         def entries = changeLogSets[i].items
@@ -8,11 +9,12 @@ def String call() {
             def entry = entries[j]
             truncated_msg = entry.msg.take(MAX_MSG_LEN)
             changeString += " - ${truncated_msg} [${entry.author}]\n"
+            changeAuthor = "${entry.author}"
         }
     }
     if (!changeString) {
         changeString = " - No new changes"
     }
     println changeString
-    return changeString
+    return changeAuthor
 }
